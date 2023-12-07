e1c1ea56848b1d2b52b2227297f829d689bffb6c
Eya Abidi <abidi.eya001@gmail.com>
2023-12-07 19:04:08 +0100

    Course and certif page updated

---
+++ src/components/layouts/certiflistingall/CertifListingAll.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './certifListingAll.styles.css';
+
+const CertifListingAll = () => {
+    return (
+        <section>
+            <h1>Certif Listing All</h1>
+        </section>
+    )
+}
+
+export default CertifListingAll;
